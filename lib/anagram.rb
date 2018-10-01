ca67da306d23040359c47657a41df4e4871ab93b
Erin Green <erin.y.green@gmail.com>
2018-10-01 18:47:54 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,7 @@ class Anagram
   
   def match(word_array)
  
-    word_array.collect do |comparison_word|
+    word_array.collect.compact do |comparison_word|
       if @word.split("").sort === comparison_word.split("").sort 
         comparison_word
       end
